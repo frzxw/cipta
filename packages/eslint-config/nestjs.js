f09e35ae93cb812460d89a7a316c66fcacfd3bf0
Fariz Wibisono <frzwibisono@gmail.com>
2026-04-12 16:13:43 +0700

    feat(config): add nestjs configurations and apply to api

---
+++ packages/eslint-config/nestjs.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import { config as baseConfig } from './base.js';
+
+/**
+ * A custom ESLint configuration for libraries that use NestJS.
+ *
+ * @type {import("eslint").Linter.Config[]} */
+export const nestJsConfig = [
+  ...baseConfig,
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off', // Optional: 'error' if strict
+    },
+  },
+];
